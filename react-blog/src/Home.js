e0ae9f9da0880914f9366c216dfc0c25630cb2b5
Vedant pople <vedantpople4@gmail.com>
2023-01-14 22:57:17 +0530

    Fetching data

--- react-blog/src/Home.js
+++ react-blog/src/Home.js
@@ -1,22 +1,21 @@
-import { useState } from 'react';
-import BlogList from './BlogList';
-
+import { useEffect, useState } from 'react';
+import BlogList from './BlogList'; 
 
 const Home = () => {
-    const [blogs, setBlogs] = useState([
-        {title:'My new website', body:'lorem ipsum...',author:'mario', id:1 },
-        {title:'Next welcome party', body:'lorem ipsum...',author:'ross', id:2},
-        {title:'Web dev tools', body:'lorem ipsum...',author:'joey', id:3 }
-    ])
-
-    const handleDelete = (id) => {
-        const newBlogs = blogs.filter(blog => blog.id !== id)
-        setBlogs(newBlogs)
-    }
+    const [blogs, setBlogs] = useState([])
+    useEffect(()=>{
+        fetch('http://localhost:8000/blogs')
+        .then(res => {
+           return res.json();
+        })
+        .then((data)=>{
+            setBlogs(data);
+        })
+    }, []);
 
     return (
         <div className="home">
-            <BlogList blogs={blogs} title="All Blogs!" handleDelete = {handleDelete} />
+            {blogs && <BlogList blogs={blogs} /> }
         </div>
     );
 } 
