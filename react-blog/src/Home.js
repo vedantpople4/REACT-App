324a89b7e21f7b548490a88daba3d66b5534b115
Vedant pople <vedantpople4@gmail.com>
2023-01-15 22:04:55 +0530

    custom hook

--- react-blog/src/Home.js
+++ react-blog/src/Home.js
@@ -1,24 +1,17 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import BlogList from './BlogList';
-
+import useFetch from './usefetch';
 
 const Home = () => {
-    const [blogs, setBlogs] = useState([
-        {title:'My new website', body:'lorem ipsum...',author:'mario', id:1 },
-        {title:'Next welcome party', body:'lorem ipsum...',author:'ross', id:2},
-        {title:'Web dev tools', body:'lorem ipsum...',author:'joey', id:3 }
-    ])
-
-    const handleDelete = (id) => {
-        const newBlogs = blogs.filter(blog => blog.id !== id)
-        setBlogs(newBlogs)
-    }
-
+    const {data: blogs, isPending, error} = useFetch('http://localhost:8000/blogs')
+    
     return (
         <div className="home">
-            <BlogList blogs={blogs} title="All Blogs!" handleDelete = {handleDelete} />
+            { error && <div>{ error }</div>}
+            { isPending && <div>Loading....</div>}
+            {blogs && <BlogList blogs={blogs} title="All Blogs!" /> }
         </div>
-    );
+    );  
 } 
  
 export default Home;
