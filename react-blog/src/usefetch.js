c4b67769db55e5422a3bec8b0ac26bec2786fbd7
Vedant pople <vedantpople4@gmail.com>
2023-01-16 06:23:19 +0530

    Route parameters

--- react-blog/src/usefetch.js
+++ react-blog/src/usefetch.js
@@ -6,24 +6,32 @@ const useFetch = (url) => {
     const [error, setError] = useState(null);
 
     useEffect(() => {
+        const abortCont = new AbortController();
+
         setTimeout(()=>{
-        fetch(url)
+        fetch(url, { signal: abortCont.signal })
         .then(res => {
             if(!res.ok){
                 throw Error('Cannot fetch data from that resource!')
             }
             return res.json();
         })
-        .then((data)=>{
+        .then(data=>{
             setData(data);
             setIsPending(false);
             setError(null); 
         })
         .catch(err=>{
+            if (err.name === 'AbortError'){
+                console.log('Fetch aborted');
+            } else {
             setIsPending(false);
             setError(err.message);
+            }
         })
         }, 1000);
+
+        return () => abortCont.abort();
     }, [url]);
 
     return {data, isPending, error}
